b501258deea8d257a59158d31712113264321ef3
James Slepicka <james@slepicka.net>
2017-12-12 22:19:35 -0600

    Add mapper 189.  Fix APU IRQ bug.

---
+++ nes/mappers/mapper189.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "mapper4.h"
+
+class c_mapper189 :
+	public c_mapper4
+{
+public:
+	c_mapper189();
+	~c_mapper189() {};
+	void WriteByte(unsigned short address, unsigned char value);
+	void reset();
+private:
+	virtual void Sync(void);
+	int reg_a;
+	int reg_b;
+};
